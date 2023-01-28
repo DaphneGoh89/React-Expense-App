f7dec0b55c03aaa9e3bb5f5fb59fa3099d397c2d
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-01-28 17:34:04 +0800

    Code cleaning

--- react-expense-app/src/store/store.js
+++
@@ -1,4 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import expenseFormReducer from "../components/ExpenseForm/expenseFormSlice";
-
-export default configureStore({ reducer: { expenseForm: expenseFormReducer } });
